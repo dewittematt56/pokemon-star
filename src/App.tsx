c6a5ce32304303a24086b038fa9747cc9dd07ec2
dewittematt56 <dewittematt@outlook.com>
2024-07-20 21:05:22 -0500

    Remove Main Menu Component
    Now a scene is used.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import PokemonStar from "./game/pokemonStar";
 function App() {
   return (
     <div className="App" style={{backgroundColor: "black", height: "100vh", width: "100vw"}}>
-      {/* <MainMenu /> */}
       <PokemonStar />
     </div>
   );
